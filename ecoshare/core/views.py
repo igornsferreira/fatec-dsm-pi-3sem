85e0d77aaa1fb1a2948b6f2e0701658f9dfe8a92
Bruno Alves <127157201+Brubzie@users.noreply.github.com>
2024-05-04 10:48:49 -0300

    Atualização das views.py
    Foram realizadas alterações no arquivo views.py, sendo elas:
    - Agora tendo classes para ser orientado a objetos
    - Adição da função que conecta ao cadastro.html

--- ecoshare/core/views.py
+++ ecoshare/core/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, redirect
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+class CadastroView(View):
+    def get(self, request):
+        return render(request, 'cadastro.html')
+
+def cadastro(request):
+    if request.method == 'POST':
+        form = CadastroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CadastroForm()
+    return render(request, 'cadastro.html', {'form': form})
