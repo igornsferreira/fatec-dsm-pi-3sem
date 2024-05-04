c10288a742b3a9746a76686a08e6b5141b95cffc
Bruno Alves <127157201+Brubzie@users.noreply.github.com>
2024-05-04 10:46:42 -0300

    Esboço do futuro admin

--- ecoshare/core/admin.py
+++ ecoshare/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import SeuModelo
 
-# Register your models here.
+""" Super Usuário
+O nome de super usuário deve ser criado
+futuramente utilizando dos comandos:
+
+python manage.py createsuperuser
+python manage.py runserver
+"""
+
+# admin.site.register(SeuModelo)
