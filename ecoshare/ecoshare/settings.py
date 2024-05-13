09d70da76522a7f1c258eabf492773aafe52cfe4
Igor Ferreira <igornsferreira@gmail.com>
2024-05-13 01:07:14 -0300

    [update] conexão com bd

--- ecoshare/ecoshare/settings.py
+++ ecoshare/ecoshare/settings.py
@@ -76,11 +76,10 @@ WSGI_APPLICATION = 'ecoshare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'ecoshare_database',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
