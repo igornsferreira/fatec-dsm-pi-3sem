05da6b1339b098c01afcbae11b162c3204fb8eed
Bruno Alves <127157201+Brubzie@users.noreply.github.com>
2024-05-22 15:44:57 -0300

    Update forms.py

--- ecoshare/core/forms.py
+++ ecoshare/core/forms.py
@@ -1,21 +1,44 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from .models import Endereco, Doacao, Usuario
+from django.contrib.auth.forms import UserCreationForm
 
 class EnderecoForm(forms.ModelForm):
     class Meta:
         model = Endereco
         fields = ['cep', 'rua', 'bairro', 'numero', 'cidade', 'estado', 'pais']
 
-class DoacaoForm(forms.ModelForm):
-    class Meta:
-        model = Doacao
-        fields = ['material_doado', 'peso', 'data', 'item_recebido', 'validacao']
+class UsuarioForm(UserCreationForm):
+    data_nascimento = forms.DateField(
+        widget=forms.DateInput(
+            format=('%d/%m/%Y'),
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Data de Nascimento',
+                'type': 'date'
+            }
+        )
+    )
 
-class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
-        fields = ['nome_completo', 'cpf', 'email', 'senha', 'telefone', 'data_nascimento', 'endereco', 'doacoes']
+        fields = ['nome_completo', 'cpf', 'email', 'telefone', 'data_nascimento', 'endereco', 'doacoes']
+        labels = {
+            'nome_completo': 'Nome Completo',
+            'cpf': 'CPF',
+            'email': 'Email',
+            'telefone': 'Telefone',
+            'data_nascimento': 'Data de Nascimento',
+            'endereco': 'Endereço',
+            'doacoes': 'Doações',
+        }
+        widgets = {
+            'nome_completo': forms.TextInput(attrs={'class': 'form-control'}),
+            'cpf': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'telefone': forms.TextInput(attrs={'class': 'form-control'}),
+            'endereco': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
     def clean_nome_completo(self):
         nome_completo = self.cleaned_data['nome_completo']
@@ -31,10 +54,6 @@ class UsuarioForm(forms.ModelForm):
     def clean_email(self):
         email = self.cleaned_data['email']
         return email
-    
-    def clean_senha(self):
-        senha = self.cleaned_data['senha']
-        return senha
 
     def clean_telefone(self):
         telefone = self.cleaned_data['telefone']
