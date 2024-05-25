b7579d860f968c557284b949a66d14123ebbcda0
Igor Ferreira <igornsferreira@gmail.com>
2024-05-25 11:05:48 -0300

    [add] recriação migrations e ajustes no html

--- ecoshare/core/migrations/0001_initial.py
+++ ecoshare/core/migrations/0001_initial.py
@@ -1,6 +1,8 @@
 import core.models
+from django.conf import settings
 import django.core.validators
 from django.db import migrations, models
+import django.db.models.deletion
 import djongo.models.fields
 
 
@@ -9,6 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
@@ -22,6 +25,10 @@ class Migration(migrations.Migration):
                 ('item_recebido', models.CharField(max_length=50)),
                 ('validacao', models.BooleanField(default=False)),
             ],
+            options={
+                'verbose_name': 'Doacao',
+                'verbose_name_plural': 'Doacoes',
+            },
         ),
         migrations.CreateModel(
             name='EnderecoModel',
@@ -35,6 +42,10 @@ class Migration(migrations.Migration):
                 ('estado', models.CharField(max_length=50)),
                 ('pais', models.CharField(max_length=50)),
             ],
+            options={
+                'verbose_name': 'Endereco',
+                'verbose_name_plural': 'Enderecos',
+            },
         ),
         migrations.CreateModel(
             name='UsuarioModel',
@@ -46,10 +57,13 @@ class Migration(migrations.Migration):
                 ('senha', models.CharField(max_length=100)),
                 ('telefone', models.CharField(max_length=15, validators=[django.core.validators.RegexValidator(regex='^\\(\\d{2}\\) \\d{4,5}-\\d{4}$')])),
                 ('data_nascimento', models.DateField()),
-                ('EnderecoModel', djongo.models.fields.EmbeddedField(model_container=core.models.EnderecoModel)),
+                ('endereco', djongo.models.fields.EmbeddedField(model_container=core.models.EnderecoModel)),
                 ('doacoes', djongo.models.fields.ArrayField(model_container=core.models.DoacaoModel)),
+                ('user', models.OneToOneField(null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
+                'verbose_name': 'Usuario',
+                'verbose_name_plural': 'Usuarios',
                 'db_table': 'ecoshare',
             },
         ),
