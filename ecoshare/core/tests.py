4b59dd2ec3b304847accd69da2aca260fae70227
William-Fonseca-Geralde <127157201+Brubzie@users.noreply.github.com>
2024-05-29 20:08:51 -0300

    Adição da função __init__

--- ecoshare/core/tests.py
+++ ecoshare/core/tests.py
@@ -2,6 +2,10 @@ from django.test import TestCase
 from .models import EnderecoModel, DoacaoModel, UsuarioModel
 
 class EnderecoVazioTest(TestCase):
+    def __init__(self, *args, **kwargs):
+        self.address = kwargs.pop('address', 'http://127.0.0.1:8000')
+        super().__init__(*args, **kwargs)
+
     def setUp(self):
         self.resp = self.client.get('/')
 
