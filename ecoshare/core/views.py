870fe08a58f2ebcb13629d6b26f7ab1e1aa0fc11
Igor Ferreira <igornsferreira@gmail.com>
2024-06-03 00:11:54 -0300

    [add] cadastro de doações

--- ecoshare/core/views.py
+++ ecoshare/core/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.views import View
 from django.shortcuts import render, redirect
-from .models import UsuarioModel  
+from .models import UsuarioModel, DoacaoModel
 from bson import ObjectId
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.forms import UserCreationForm
@@ -102,13 +102,63 @@ class RelatorioClienteView(View):
 
     def get(self, request):
         return render(request, self.template_name)
-
-class DoacoesClienteView(View):
-    template_name = 'doacoesCliente.html'
     
+class DoacoesClienteView(LoginRequiredMixin, View):
+    template_name = 'doacoesCliente.html'
+
     def get(self, request):
         return render(request, self.template_name)
 
+    def post(self, request):
+        # Obtendo os dados do formulário
+        material_doado = request.POST.get('material')
+        peso = request.POST.get('peso')
+        data = request.POST.get('data')
+        item_recebido = request.POST.get('brinde')
+
+        # Criando uma nova instância de DoacaoModel
+        doacao = DoacaoModel(
+            _id=ObjectId(),
+            material_doado=material_doado,
+            peso=peso,
+            data=data,
+            item_recebido=item_recebido,
+            validacao=False  # Sempre inicializado como falso
+        )
+
+        # Salvando a nova doação
+        doacao.save()
+
+        # Converte a instância de doação para um dicionário
+        doacao_dict = {
+            "_id": doacao._id,
+            "material_doado": doacao.material_doado,
+            "peso": doacao.peso,
+            "data": doacao.data,
+            "item_recebido": doacao.item_recebido,
+            "validacao": doacao.validacao
+        }
+
+        try:
+            # Obtendo o usuário
+            usuario = UsuarioModel.objects.get(user=request.user)
+            
+            # Usando o método update para adicionar a doação
+            UsuarioModel.objects.filter(user=request.user).update(
+                doacoes=[*usuario.doacoes, doacao_dict]
+            )
+
+        except UsuarioModel.DoesNotExist:
+            # Se o usuário não existir, criar um novo
+            usuario = UsuarioModel(
+                user=request.user,
+                doacoes=[doacao_dict]
+            )
+            usuario.save()
+
+        # Redirecionando de volta para a página de doações
+        return HttpResponseRedirect(reverse('doacoesCliente'))
+        
 class BrindesClienteView(View):
     template_name = 'brindesCliente.html'
 
