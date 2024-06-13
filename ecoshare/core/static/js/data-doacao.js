6e469b8af89dd445ee7e3c14ee0cb83ffdc1222c
Beatriz Machado de Oliveira <121825810+beamclive@users.noreply.github.com>
2024-06-12 22:59:09 -0300

    Ajuste do fuso horário da data da doação e inclusão de empresas parceiras

--- ecoshare/core/static/js/data-doacao.js
+++ ecoshare/core/static/js/data-doacao.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const dateField = document.getElementById('data');
-    const today = new Date().toISOString().split('T')[0];
-    dateField.value = today;
+
+    const now = new Date();
+    const options = { timeZone: 'America/Sao_Paulo', year: 'numeric', month: '2-digit', day: '2-digit' };
+    const brazilDate = new Intl.DateTimeFormat('en-CA', options).format(now);
+
+    dateField.value = brazilDate;
     dateField.readOnly = true;
 });
